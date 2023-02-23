eb1fd1a73dbb1c4413fd8aeafad552a33e20a43c
Isabelle <meguminperson@gmail.com>
2023-02-23 10:41:42 +0000

    0.0.3

--- scrumpy/complier.py
+++ scrumpy/complier.py
@@ -2,6 +2,11 @@ from typing import (
     Any,
 )
 
+OPERATIONS = [
+    "+", 
+    "-",
+]
+
 VARS = {}
 
 def run(filepath: str) -> None:
@@ -15,13 +20,28 @@ def run(filepath: str) -> None:
 def _check_syntax(content: str) -> bool:
     return True
 
-def _decode_var_value(var_value) -> Any:
+def _decode_value(value: Any) -> None:
+    if "." in str(value):
+        var_name, var_index = value.split(".")
+        var_index = _decode_value(var_index)
+        return _decode_value(VARS[var_name][var_index])
+    elif value in VARS:
+        return _decode_value(VARS[value])
+    else:
+        return int(value)
+
+
+def _decode_var_value(var_value: Any) -> Any:
     var_value_list_segments = var_value.split(", ")
     if len(var_value_list_segments) == 1:
         if var_value_list_segments[0][-1] == ",":
-            return var_value_list_segments[:-1]
+            print(f"{var_value_list_segments=}")
+            return var_value_list_segments[0][:-1]
     if len(var_value_list_segments) > 1:
-        return var_value_list_segments
+        temp = []
+        for segment in var_value_list_segments:
+            temp.append(_decode_value(segment))
+        return temp
     return var_value_list_segments[0]
 
 def _assign_var(var_name: str, var_value: Any) -> None:
@@ -30,16 +50,36 @@ def _assign_var(var_name: str, var_value: Any) -> None:
     print(f"{var=}")
     if type(var) == list:
         return
-    values = var.split(" ")
-    values_iter = iter(values)
-    if not len(values) % 3:
-        for i in range(len(values)):
-            value1 = next(values_iter)
-            operation = next(values_iter)
-            value2 = next(values_iter)
-            print("")
-            # print(f"{values=}")
-            # print(f"{next(values_iter)=}")
+    var_summation = None
+    var_previous = None
+    var_next = None
+    var_operation = None
+    for i, v in enumerate(var.split(" ")):
+        if v in OPERATIONS:
+            var_operation = v
+            var_next = _decode_value(var.split(" ")[i+1])
+            var_previous = var_summation or _decode_value(var.split(" ")[i-1])
+            if var_operation == "+":
+                if var_summation is None:
+                    var_summation = var_previous + var_next
+                else: 
+                    var_summation += var_next
+                # print(f"{var_summation=}\n{var_operation=}\n{var_next=}\n{var_previous=}")
+            print(f"{var_operation=}\n{var_next=}\n{var_previous=}\n{var_summation=}\n---")
+    if var_operation is not None:
+        VARS[var_name] = var_summation
+    # if 
+    # values = var.split(" ")
+    # values_iter = iter(values)
+    # if len(values) >= 3:
+    #     for i in range(int(len(values)/3)):
+    #         print(values)
+    #         value1 = next(values_iter)
+    #         operation = next(values_iter)
+    #         value2 = next(values_iter)
+    #         print(f"{value1=}\n{operation=}\n{value2=}")
+    #         # print(f"{values=}")
+    #         # print(f"{next(values_iter)=}")
 
     # segments_if_list = var_value.split(".")
     # if len(segments_if_list) > 1:
@@ -51,13 +91,28 @@ def _assign_var(var_name: str, var_value: Any) -> None:
     #         var_value = _decode_var_value(VARS[list_name][int(index)])
     # VARS[var_name] = _decode_var_value(var_value)
 
+def _fully_decode(item: str) -> int:
+    while True:
+        output = _decode_value(item)
+        if output == item:
+            return output
+        else:
+            item = output
+
 def _output(output: str) -> None:
     if output in VARS:
-        return print(VARS[str(output)])
-    print(f"{VARS=}")
-    print(f"{output=}")
+        output = VARS[str(output)]
+    final_out = ""
+    if isinstance(output, list):
+        for item in output:
+            final_out += f"{_fully_decode(item)}, "
+        final_out = final_out[:-2]
+    else:
+        final_out = _fully_decode(output)
+    return print(f"OUT: {final_out}")
 
 def _unpack_line(line: str) -> None:
+    if line.startswith("#"): return
     line_segments_if_var = line.split(" = ")
     if len(line_segments_if_var) == 2:
         return _assign_var(line_segments_if_var[0], line_segments_if_var[1])
